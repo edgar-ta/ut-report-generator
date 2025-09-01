3041f2348ed0b2f8cf17fd623d6a2c5c402b691d
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2025-08-31 21:17:46 -0600

    Primera versión de script de instalación para Windows. No funciona xd

--- python-app/main.py
+++ python-app/main.py
@@ -5,8 +5,10 @@ import routes.image_slide as image_slide
 import routes.report as report
 
 from flask import Flask, request, jsonify
+from typing import TypeVar
 
 import logging
+import sys
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
@@ -17,6 +19,20 @@ app.register_blueprint(report.blueprint)
 app.register_blueprint(pivot_table.blueprint)
 app.register_blueprint(image_slide.blueprint)
 
+T = TypeVar("T")
+def item_or(_list: list[T], index: int, default: T) -> T:
+    try:
+        return _list[index]
+    except IndexError:
+        return default
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='logs.log')
-    app.run()
+    mode = item_or(_list=sys.argv, index=1, default="dev")
+    port = int(item_or(_list=sys.argv, index=2, default="5000"))
+
+    if mode == "dev":
+        logging.basicConfig(filename='logs.log')
+        app.run(port=port)
+    elif mode == "release":
+        from waitress import serve
+        serve(app=app, port=port)
